95d4ffac6cff6b2f2eca330a951418815b34e6a3
Lody <lodyiaremko@proton.me>
2025-10-22 19:18:06 +0200

    Bad note edit mode controller logic. To fix.

--- src/main/java/com/notes/notes_app/controller/httpController.java
+++ src/main/java/com/notes/notes_app/controller/httpController.java
@@ -44,6 +44,13 @@ public class httpController {
         return "new-note";
     }
 
+    @PostMapping("/note")
+    public String updateNote(@ModelAttribute NoteEntity noteEntity, Model model) {
+        NoteEntity note = noteRepository.findById(noteEntity.getId()).get();
+        model.addAttribute("note", note);
+        return "redirect:/new-note";
+    }
+
     @PostMapping("/submit")
     public String postNote(@ModelAttribute NoteEntity noteEntity) {
         noteEntity.setPinned(false);
